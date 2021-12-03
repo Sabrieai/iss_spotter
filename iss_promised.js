1dcd392731c0b88926a9ba327f948791eca5fca8
Aisha Sabrie <sabriealt@gmail.com>
2021-12-03 13:46:31 -0500

    made small modification

--- iss_promised.js
+++ iss_promised.js
@@ -4,7 +4,7 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
-const fetchCoordsByIP = function (body) {
+const fetchCoordsByIP = function(body) {
   const ip = JSON.parse(body).ip;
   return request(`https://freegeoip.app/json/${ip}`);
 };
